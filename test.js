77defd15f92e3eb569c2a75ec5332453adce627d
AwesomePermissions <awesomepermissions@gmail.com>
2023-10-23 22:34:14 +0200

    fix: apiUrl condition error

--- test.js
+++ test.js
@@ -8,53 +8,57 @@ const AwesomePermissions = require('./lib');
         });
 
         console.log("1");
-        await awp.addPermissionsToItems({
+        const r1 = await awp.addPermissionsToItems({
             'items': ['Leonardooooooodoode'],
             'itemTypes': ['user'],
             'permissions': ['image.mona-lisa'],
             'actions': ['owner']
         });
+        console.log('1 - ', r1);
         console.log("2");
-        await awp.addPermissionsToItems({
+        const r2 = await awp.addPermissionsToItems({
             'items': ['Mona Lisa´s'],
             'itemTypes': ['image'],
             'permissions': ['image.mona-lisa'],
             'actions': ['owner']
         });
+        console.log('2 - ', r2);
         console.log("3");
-        await awp.addPermissionsToItems({
+        const r3 = await awp.addPermissionsToItems({
             'items': ['Mona Lisa´s'],
             'itemTypes': ['image'],
             'permissions': ['image.mona-lisa'],
             'actions': ['view']
         });
+        console.log('3 - ', r3);
         console.log("4");
-        await awp.addGroups([{
+        const r4 = await awp.addGroups([{
             'name': 'Users with image view acces',
             'key': 'users-imagee'
         }]);
+        console.log('4 - ', r4);
         console.log("5");
-        await awp.addItemsToGroups({
+        const r5 = await awp.addItemsToGroups({
             'groups': ['users-imagee'],
             'items': ['additional-user-1', 'additional-user-2', 'additional-user-3'],
             'itemTypes': ['user']
         });
+        console.log('5 - ', r5);
         console.log("6");
-        await awp.addPermissionsToGroups({
+        const r6 = await awp.addPermissionsToGroups({
             'groups': ['users-imagee'],
             'permissions': ['image.mona-lisa'],
             'actions': ['view']
         });
+        console.log('6 - ', r6);
         console.log("7");
         // We check that they have access to each other by removing all items that have access to the image.
-        const response = await awp.getItemsWithPermissionsForItems({
+        const r7 = await awp.getItemsWithPermissionsForItems({
             'items': ['Mona Lisa´s'],
             'itemTypes': ['user']
         });
-
-        console.log(response);
-
-        console.log(JSON.stringify(response, null, ' '));
+        console.log('7 - ');
+        console.log(JSON.stringify(r7, null, ' '));
 
 
         /*
